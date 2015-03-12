8ae22a272142f0181febbd2aa95d9fdc5eaaacbf
FRAMCAD-Michael <yuqian.m.lu@gmail.com>
2015-03-12 16:51:57 +1300

    TDD WIP

---
+++ OdeToFood/OdeToFood.Tests/Features/RestaurantRater.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OdeToFood.Models;
+
+namespace OdeToFood.Tests.Features
+{
+    class RestaurantRater
+    {
+        private Restaurant _restaurant;
+
+        public RestaurantRater(Restaurant restaurant)
+        {
+            // TODO: Complete member initialization
+            this._restaurant = restaurant;
+        }
+
+        public RatingResult  ComputeRating(int numberOfReviews)
+        {
+            var result = new RatingResult();
+            result.Rating = (int)_restaurant.Reviews.Average(r=>r.Rating);
+            return result;
+        }
+
+        public RatingResult ComputeWeightedRate (int numberOfReviews)
+        {
+            var reviews = _restaurant.Reviews.ToArray();
+            var result = new RatingResult();
+            var counter = 0;
+            var total = 0;
+            for (int i = 0; i < reviews.Count(); i++)
+            {
+                if (i<reviews.Count()/2)
+                {
+                    counter += 2;
+                    total += reviews[i].Rating;
+                }
+                else
+                {
+                    counter += 1;
+                    total += reviews[i].Rating;
+                }
+            }
+            result.Rating = total / counter;
+            return result;
+        }
+    }
+}
